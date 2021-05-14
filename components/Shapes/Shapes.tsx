00461af7a10085357155b90d7141cd69a926c2ba
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-05-14 20:49:05 +0530

    code corrections

--- components/Shapes/Shapes.tsx
+++ components/Shapes/Shapes.tsx
@@ -6,12 +6,16 @@ import "codemirror/mode/javascript/javascript";
 
 const Circle = () => {
   const styleSheet = `
-  .circle {
+  .dimension {
     width: 100px;
     height: 100px;
-    border-radius: 50%;
+  }
+  .bg-color {
     background-color: red;
- }`;
+  }
+  .radius {
+    border-radius: 50%;
+  }`.trim();
 
   return (
     <div id="circle">
@@ -22,7 +26,7 @@ const Circle = () => {
         }}
       />
       <div id="html">
-        <div className="circle"></div>
+        <div className="dimension bg-color radius"></div>
       </div>
     </div>
   );
@@ -44,34 +48,45 @@ const CodePreviewer = ({ html, css }) => {
 
 function CodeEditor({ code, setCode }: any) {
   return (
-    <div>
-      <CodeMirror
-        value={code?.html?.trim()}
-        options={{
-          mode: "xml",
-          theme: "material",
-          lineNumbers: true
-        }}
-        onBeforeChange={(editor, data, value) => {
-          code.html = value;
-          setCode({ ...code });
-        }}
-        onChange={(editor, data, value) => {}}
-      />
-      <hr />
-      <CodeMirror
-        value={code?.css?.replace(/        /g, " ")}
-        options={{
-          mode: "css",
-          theme: "material",
-          lineNumbers: true
-        }}
-        onBeforeChange={(editor, data, value) => {
-          code.css = value;
-          setCode({ ...code });
-        }}
-        onChange={(editor, data, value) => {}}
-      />
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between"
+      }}
+    >
+      <div style={{ width: "50%" }}>
+        <h5 style={{ margin: 0 }}>HTML</h5>
+        <CodeMirror
+          value={code?.html?.trim()}
+          options={{
+            mode: "xml",
+            theme: "material",
+            lineNumbers: true
+          }}
+          onBeforeChange={(editor, data, value) => {
+            code.html = value;
+            setCode({ ...code });
+          }}
+          onChange={(editor, data, value) => {}}
+        />
+      </div>
+      <div style={{ width: `calc(50% - 4px)` }}>
+        <h5 style={{ margin: 0 }}>CSS</h5>
+        <CodeMirror
+          value={code?.css?.replace(/        /g, " ")}
+          options={{
+            mode: "css",
+            theme: "material",
+            lineNumbers: true
+          }}
+          onBeforeChange={(editor, data, value) => {
+            code.css = value;
+            setCode({ ...code });
+          }}
+          onChange={(editor, data, value) => {}}
+        />
+      </div>
     </div>
   );
 }
@@ -109,17 +124,18 @@ function Shapes() {
 
   return (
     <>
+      {components.map((x, idx) => (
+        <button key={`btn-${idx}`} onClick={() => renderComponent(x)}>
+          {x.name}
+        </button>
+      ))}
+      <hr />
       <div style={{ visibility: "hidden", height: 0 }}>
         {activeComponent?.component || <></>}
       </div>
       <CodePreviewer html={code?.html} css={code?.css} />
       <hr />
       <CodeEditor code={code} setCode={setCode} />
-      {components.map((x, idx) => (
-        <button key={`btn-${idx}`} onClick={() => renderComponent(x)}>
-          {x.name}
-        </button>
-      ))}
     </>
   );
 }
